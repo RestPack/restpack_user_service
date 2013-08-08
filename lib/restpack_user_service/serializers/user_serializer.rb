9d1651cdcf173df3ca99be72f769585ce7ae0132
Gavin Joyce <gavinjoyce@gmail.com>
2013-08-08 13:27:11 +0100

    added user serializer

---
+++ lib/restpack_user_service/serializers/user_serializer.rb
@@ -0,0 +1,14 @@
+module RestPack::Activity::Service::Serializers
+  class UserSerializer
+    include RestPack::Serializer
+
+    self.model_class = RestPack::Activity::Service::Models::User
+    self.key = :users
+
+    attributes :id, :channel_id, :description, :email, :image, :avatar, :location, :name, :nickname, :url
+
+    def avatar
+      image || "http://robohash.org/#{id}.png?size=200x200"
+    end
+  end
+end
